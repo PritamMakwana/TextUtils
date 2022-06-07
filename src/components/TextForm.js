eb65fbbbc1651d24333a32345f9e8274278f0b97
PritamMakWana <pritammakaWana17561@gmail.com>
2022-06-07 18:11:27 +0530

    add drak mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,12 +15,24 @@ export default function TextForm(props) {
     }
 
     const headleInitClick = () => {
-        // console.log("initCap  was clicked " + text)
-        
+
         let newText = text.toLowerCase().replace(/(?:^|\s)[a-z]/g, function (m) {
             return m.toUpperCase();
-         });
+        });
         setText(newText)
+    }
+
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+    }
+
+
+    const headleclearClick = () => {
+        let newText = '';
+        setText(newText)
+        console.log("clear: " + text);
     }
 
 
@@ -29,6 +41,13 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
+    const headleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(" "))
+    }
+
+
+
     const [text, setText] = useState("")
 
     // text = (new Text)  wrong way to change text
@@ -36,23 +55,27 @@ export default function TextForm(props) {
 
     return (
         <>
-            <div className="container">
+            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={headleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={headleOnChange} id="myBox" rows="8" style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }}></textarea>
                 </div>
-                <button className="btn btn-primary mx-3" onClick={headleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-3" onClick={headleLoClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-3" onClick={headleInitClick}>Convert to initCap</button>
+                <button className="btn btn-primary mx-3" onClick={headleUpClick}>Uppercase</button>
+                <button className="btn btn-primary mx-3" onClick={headleLoClick}>Lowercase</button>
+                <button className="btn btn-primary mx-3" onClick={headleInitClick}>Capitalize Word</button>
+                <button type="submit" onClick={speak} className="btn btn-primary mx-3">Speak</button>
+                <button className="btn btn-primary mx-3 " onClick={headleExtraSpaces}>Remove Extra Spaces</button>
+                <button className="btn btn-primary mx-3 " onClick={headleclearClick}>All Clear</button>
+
             </div>
 
-            <div className="container my-3">
+            <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h2>Your Text Summary</h2>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
-                
+                <p className="preview">{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
+
 
             </div>
         </>
